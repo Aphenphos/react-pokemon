6949ab3999bc9689b3372785810537f3fd20aa88
Aphenphos <MaxwillWinters@gmail.com>
2022-09-21 18:53:21 -0400

    slightly better style

--- src/components/PokeCard/PokeCard.js
+++ src/components/PokeCard/PokeCard.js
@@ -2,8 +2,7 @@ import './PokeCard.css';
 
 export default function PokeCard({ pokemon, type_1, type_2, url_image }) {
   return (
-    <div className="poke-card">
-      <img className="poke-img" src={url_image} ></img>
+    <div className="poke-card" style={{ backgroundImage: `url(${url_image})` }}>
       <h1 className="poke-name">{pokemon}</h1>
       <h1 className="poke-type1">{type_1}</h1>
       <h1 className="poke-type2">{type_2}</h1>
